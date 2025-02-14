bd9211378fb5aa2a9260c5547dc8bd64668b3a82
Icee <159829566+Icee12321@users.noreply.github.com>
2025-02-14 23:02:18 +0300

    added gitignore

--- obj/Debug/net9.0/MarketRPG.AssemblyInfo.cs
+++ obj/Debug/net9.0/MarketRPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dd93a01401eedcf40daf1991abb91c277eee6ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
